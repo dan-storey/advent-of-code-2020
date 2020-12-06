d9e6cd0a3e9659ace3eede44d19d45b1c106ad47
Dan Storey <dan_storey@hotmail.co.uk>
2020-12-06 20:19:05 +0000

    Day 6 optimisation.

--- src/main/kotlin/net/dstack/aoc/day6/Day6.kt
+++ src/main/kotlin/net/dstack/aoc/day6/Day6.kt
@@ -7,8 +7,11 @@ class Day6 {
     }
 
     fun countYesPart2(answers: List<String>): Int {
-        return answers.map { it.split(' ') }
-                .map { group -> ('a'..'z').filter { question -> group.all { it.contains(question) } }.size }
-                .sum()
+        var count = 0
+        for (groupAnswers in answers) {
+            val questions = groupAnswers.replace(" ", "").toCharArray().distinct()
+            count += questions.filter { question -> groupAnswers.split(' ').all { it.contains(question) } }.size
+        }
+        return count
     }
 }
