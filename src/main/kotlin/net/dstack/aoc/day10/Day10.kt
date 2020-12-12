aa851d852f8b31795ff8ffa9f9ffcc6c3fc9616c
Dan Storey <dan_storey@hotmail.co.uk>
2020-12-12 18:34:43 +0000

    Tidy up.

--- src/main/kotlin/net/dstack/aoc/day10/Day10.kt
+++ src/main/kotlin/net/dstack/aoc/day10/Day10.kt
@@ -18,15 +18,13 @@ class Day10 {
     fun part2(adapters: List<Int>): Long {
         val sortedAdapters = adapters.sorted()
         val branches = mutableMapOf<Int, Long>()
-        var branchCount = 1L
         for (i in sortedAdapters.indices.reversed()) {
-            val adapter = sortedAdapters[i]
-            branchCount += (i + 2..min(i + 3, sortedAdapters.size - 1))
-                    .filter { sortedAdapters[it] - adapter <= 3 }
-                    .map { branches[sortedAdapters[it]]!!.toLong() }
+            branches[i] = (i + 2..min(i + 3, sortedAdapters.size - 1))
+                    .filter { sortedAdapters[it] - sortedAdapters[i] <= 3 }
+                    .map { branches[it]!! }
                     .sum()
-            branches[adapter] = branchCount
+                    .plus(branches.getOrDefault(i + 1, 1L))
         }
-        return branchCount
+        return branches[0]!!
     }
 }
